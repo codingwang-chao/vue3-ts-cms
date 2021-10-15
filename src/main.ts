bf9e41652641765d28bf69227c4ea20fff3bfc3a
wangchao <wangchao@xhecloud.com>
2021-10-15 15:28:19 +0800

    feat: 格式化样式

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import 'normalize.css'
+import './assets/css/index.less'
+
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
